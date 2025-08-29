f84a0b59a718e362611a11db2fa2c3db1b2f3910
Rasmus Bremholm <rasmus.brem@gmail.com>
2025-08-29 15:02:31 +0200

    Updates to DocPage

--- sanity/schemas/docPage.ts
+++ sanity/schemas/docPage.ts
@@ -1,20 +1,23 @@
-import {defineType, defineField} from 'sanity'
+import { defineType, defineField } from "sanity";
 
 export default defineType({
-  name: 'docPage',
-  title: 'Doc Page',
-  type: 'document',
-  fields: [
-    defineField({ name: 'title', type: 'string', validation: r => r.required() }),
-    defineField({ name: 'slug', type: 'slug', options: { source: 'title' }, validation: r => r.required() }),
-    defineField({ name: 'order', type: 'number' }),
-    defineField({
-      name: 'content',
-      type: 'array',
-      of: [
-        { type: 'block' },
-        { type: 'code', options: { withFilename: true } },
-      ],
-    }),
-  ],
-})
+	name: "docPage",
+	title: "Doc Page",
+	type: "document",
+	options: { list: ["guide", "howto", "reference"] },
+	initialValue: "guide",
+	preview: {
+		select: { title: "title", subtitle: "slug.current" },
+	},
+	orderings: [{ title: "Order (asc)", name: "orderAsc", by: [{ field: "order", direction: "asc" }] }],
+	fields: [
+		defineField({ name: "title", type: "string", validation: (r) => r.required() }),
+		defineField({ name: "slug", type: "slug", options: { source: "title" }, validation: (r) => r.required() }),
+		defineField({ name: "order", type: "number" }),
+		defineField({
+			name: "content",
+			type: "array",
+			of: [{ type: "block" }, { type: "code", options: { withFilename: true } }],
+		}),
+	],
+});
