be674b3064645d204111e89d57e588fa6d2a10bb
Rasmus Bremholm <rasmus.brem@gmail.com>
2025-08-29 11:09:16 +0200

    Studio is working

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,14 +1,15 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import schemas from './sanity/schemas'
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { visionTool } from "@sanity/vision";
+import schemas from "./sanity/schemas";
+import { codeInput } from "@sanity/code-input";
 
 export default defineConfig({
-  name: 'default',
-  title: 'Heresy Docs',
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
-  basePath: '/studio',
-  plugins: [structureTool(), visionTool()],
-  schema: { types: schemas },
-})
+	name: "default",
+	title: "Heresy Docs",
+	projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+	dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+	basePath: "/studio",
+	plugins: [structureTool(), visionTool(), codeInput()],
+	schema: { types: schemas },
+});
