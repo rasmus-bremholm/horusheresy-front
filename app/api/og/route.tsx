eef9aa062abf8774ed77850877551199a7738adb
Rasmus Bremholm <rasmus.brem@gmail.com>
2025-09-14 23:09:21 +0200

    Update route.tsx

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -2,11 +2,17 @@
 
 import { ImageResponse } from "@vercel/og";
 
+const fontUrl = 'https://fonts.googleapis.com/css2?family=IM+Fell+English:wght@400&display=swap';
+
 export async function GET(req: Request) {
 	// This generates a legion specific OG image based on the data i fetch. Like legion name and number.
 	const { searchParams } = new URL(req.url);
 	const legion = searchParams.get("legion");
 	const legionNumber = searchParams.get("number") || "XX";
+
+	const fontData = await fetch(
+		new URL('https://fonts.gstatic.com/s/imfellenglish/v18/Ktk6ALSMeZjqPXneuFCgdg.woff2')
+	).then((res) => res.arrayBuffer());
 
 	if (!legion) {
 		return new ImageResponse(
@@ -21,7 +27,7 @@ export async function GET(req: Request) {
 						justifyContent: "center",
 						flexDirection: "column",
 						color: "whitesmoke",
-						fontFamily: "serif",
+						fontFamily: "IM Fell English",
 					}}>
 					<h1
 						style={{
@@ -32,7 +38,18 @@ export async function GET(req: Request) {
 					</h1>
 					<p style={{ fontSize: "32px", margin: 0, opacity: 0.8 }}>The 30K REST API</p>
 				</div>
-			)
+			),
+			{
+				width: 1200,
+				height: 630,
+				fonts: [
+					{
+						name: 'IM Fell English',
+						data: fontData,
+						style: 'normal',
+					},
+				],
+			}
 		);
 	}
 
@@ -48,18 +65,28 @@ export async function GET(req: Request) {
 					justifyContent: "center",
 					flexDirection: "column",
 					color: "whitesmoke",
-					fontFamily: "serif",
+					fontFamily: "IM Fell English",
 				}}>
 				<h1
 					style={{
-						fontSize: "48px",
+						fontSize: "56px",
 						margin: "0 0 20px 0",
 					}}>
 					{legion}
 				</h1>
-				<p style={{ fontSize: "32px", margin: 0, opacity: 0.8 }}>The {legionNumber} Legion</p>
+				<p style={{ fontSize: "48px", margin: 0, opacity: 0.8 }}>The {legionNumber} Legion</p>
 			</div>
 		),
-		{ width: 1200, height: 630 }
+		{
+			width: 1200,
+			height: 630,
+			fonts: [
+				{
+					name: 'IM Fell English',
+					data: fontData,
+					style: 'normal',
+				},
+			],
+		}
 	);
 }
